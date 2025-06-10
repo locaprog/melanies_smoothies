aff8d9177d6aa2ccd7c39285f76c312541a858fb
locaprog <martha.clark@pta.wa.gov.au>
2025-06-10 16:03:11 +0800

    Create streamlit_app.py
    copied from my Custom Smoothie SIS app

---
+++ streamlit_app.py
@@ -0,0 +1,56 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write("Choose the fruits you want in your custom smoothie")
+
+
+#LAB 4 : Add an order name and assign it to a variable
+st.subheader(":exclamation: LAB 4 - Add name of order")
+
+# add a text box
+var_name_of_order = st.text_input('Name of Order:')
+st.write('The name of your order will be:', var_name_of_order)
+
+# LAB 2: create multiselect and show selection
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select (col('fruit_name'))
+
+st.subheader(":exclamation: LAB 2 - Multiselect")
+
+ingredients_list = st.multiselect ('Choose up to 5 ingredients:', my_dataframe, max_selections =5)
+
+if ingredients_list:
+    #show the list
+    st.write(ingredients_list)
+    st.text(ingredients_list)
+    
+    ingredients_string=''   
+
+    for var_fruit_chosen in ingredients_list:
+        ingredients_string +=   var_fruit_chosen + ' '
+    
+    #show the variable  content
+    #st.write(ingredients_string)
+
+    # create a sql statement with python
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + var_name_of_order + """')"""
+
+    #show the sql statement
+    #st.write(my_insert_stmt)
+
+    #run the code up to here
+    #st.stop()
+
+    #Now we add a button
+    time_to_insert = st.button('Submit Order')
+
+    #catch the click of the button and execute the sql 
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+
+        st.success("Your Smoothie is ordered, " + var_name_of_order)
