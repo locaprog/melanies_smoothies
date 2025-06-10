0f0b8e066a16564857c01366237e000e159a4c96
locaprog <martha.clark@pta.wa.gov.au>
2025-06-10 16:57:14 +0800

    Update streamlit_app.py
    Added json extension to the response

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ if ingredients_list:
 
         st.success("Your Smoothie is ordered, " + var_name_of_order)
 
+#New section to display smoothiefroot nutrition info
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json)
