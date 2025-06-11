10b0b14c43c45af50703adeec21ff5f9167a9d7e
locaprog <martha.clark@pta.wa.gov.au>
2025-06-11 10:07:45 +0800

    Update streamlit_app.py
    fixed issue the variable call : var_fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ if ingredients_list:
     for var_fruit_chosen in ingredients_list:
         ingredients_string +=   var_fruit_chosen + ' '
 
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == var_fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', var_fruit_chosen,' is ', search_on, '.')
 
         st.subheader(var_fruit_chosen + ' Nutrition Information')
         
