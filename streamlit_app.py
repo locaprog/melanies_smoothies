d7239485845d032e723f9d7d57eaabce425d6783
locaprog <martha.clark@pta.wa.gov.au>
2025-06-11 10:05:32 +0800

    Update streamlit_app.py
    small change

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ pd_df = my_dataframe.to_pandas()
 # st.dataframe(pd_df)
 # st.stop()                                                                       
 
-st.subheader(":exclamation: LAB 2 - Multiselect")
+st.subheader(":exclamation:  Multiselect")
 
 ingredients_list = st.multiselect ('Choose up to 5 ingredients:', my_dataframe, max_selections =5)
 
